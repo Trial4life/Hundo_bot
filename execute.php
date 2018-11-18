464af2ad492ccf52bfb7d3498f7a52a1da7386bc
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-11-18 02:37:56 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -21,20 +21,16 @@ $text = strtolower($text);
 header("Content-Type: application/json");
 $response = '';
 
-/*$apiToken = "721221790:AAFvEHkZQUVU3S9SeNaYoKIHPvvVojnCq6Q";
+$apiToken = "721221790:AAFvEHkZQUVU3S9SeNaYoKIHPvvVojnCq6Q";
 
 $data = [
     'chat_id' => '@cento42',
     'text' => 'Hello world!'
 ];
 
-$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
-
-// Do what you want with result
-*/
 if(strpos($text, "/hundo") === 0 )
 {
-	$response = "100.";
+	$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
 }
 
 
