a35f1fa789c18a1ca2d87398ea763b5252ae6e23
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-11-18 17:00:48 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -75,7 +75,6 @@ $conn->close();
 
 
 // DEBUG - PRINT
-$response = $chatId;
 $parameters = array('chat_id' => $chatId, "text" => $response, "parse_mode" => "markdown");
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
