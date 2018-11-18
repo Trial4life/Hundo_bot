262ac0236ca75c30ce09fe6bcaf4504205e92a75
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-11-18 19:42:54 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -58,7 +58,7 @@ if(strpos($text, "/100") === 0 )
 	{
 		$data = [
     		'chat_id' => $userId,
-    		'text' => 'Mandami la posizione di '.$reply.'.',
+    		'text' => 'Mandami la posizione di*'.$reply.'*.',
 		];
 		/*
 		$location = [
@@ -74,7 +74,7 @@ if(strpos($text, "/100") === 0 )
 	{
 		$data = [
    	 	'chat_id' => $userId,
-   	 	'text' => 'Mandami la posizione di '.str_replace('/100', '', $text).'.',
+   	 	'text' => 'Mandami la posizione di*'.str_replace('/100', '', $text).'*.',
 		];
 		/*
 		$location = [
