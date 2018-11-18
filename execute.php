61d7571cfb38ebd4684afbfa4bbde8a9fc083c34
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-11-18 03:03:00 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -35,7 +35,7 @@ if(strpos($text, "/hundo") === 0 )
 	$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
 }
 
-$response = $message;
+$response = $message['replytomessage']['text'];
 
 $parameters = array('chat_id' => $chatId, "text" => $response, "parse_mode" => "markdown");
 $parameters["method"] = "sendMessage";
