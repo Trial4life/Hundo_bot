973cb362188c32381b2735fd5abb844aef00d2f8
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-11-18 02:14:35 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -2,6 +2,25 @@
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 
+if(!$update)
+{
+  exit;
+}
+
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$userId = isset($message['from']['id']) ? $message['from']['id'] : "";
+$firstname = isset($message['from']['first_name']) ? $message['from']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['from']['username']) ? $message['from']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+$text = trim($text);
+$text = strtolower($text);
+header("Content-Type: application/json");
+$response = '';
+
 $apiToken = "721221790:AAFvEHkZQUVU3S9SeNaYoKIHPvvVojnCq6Q";
 
 $data = [
