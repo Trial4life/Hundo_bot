ceb0ee9854e8ef9a4bf28a936d6e896405a0013b
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-11-18 02:05:36 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -2,6 +2,17 @@
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 
+$apiToken = "721221790:AAFvEHkZQUVU3S9SeNaYoKIHPvvVojnCq6Q";
+
+$data = [
+    'chat_id' => '@cento42',
+    'text' => 'Hello world!'
+];
+
+$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
+
+// Do what you want with result
+
 if($text == "/hundo")
 {
 	$response = "100."; }
