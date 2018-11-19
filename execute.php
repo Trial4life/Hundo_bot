d74fa1ab3d7ba527c6d5d2e1674131e6d49b93f5
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-11-19 21:51:36 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -155,6 +155,7 @@ elseif($status == 0)
 
 				$query = "SELECT * FROM `quests` WHERE `pokestop` = '$pkst'";
 				$result = mysqli_query($conn,$query);
+				$row = mysqli_fetch_assoc($result);
 				if(!$result) {
 					$query = "SELECT * FROM `pokestops` WHERE `pokestop` = '$pkst'";
 					$result = mysqli_query($conn,$query);
@@ -184,7 +185,7 @@ elseif($status == 0)
 				else {
 					// AVVISO DI QUEST GIÀ SEGNALATA
 					//$response = 'La quest di questo pokéstop è stata già segnalata per oggi. DEBUG: '.!$result;
-					$response = $result;
+					$response = $row;
 					$parameters = array('chat_id' => $chatId, "text" => $response, "parse_mode" => "markdown");
 					$parameters["method"] = "sendMessage";
 					echo json_encode($parameters);
