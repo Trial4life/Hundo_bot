bbafc85f027a2a606eeaf4ff6ee1d3a7c999a819
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-11-19 18:02:17 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -32,7 +32,8 @@ $channel = '@centoPoGO';
 $bot_Exeggutor = 158754689;
 $group_PogoTube42 = -1001204753064;
 $group_NordEstLegit = -1001119443518;
-$authorizedChats = array( $group_PogoTube42, $group_NordEstLegit, $bot_Exeggutor );
+$group_admin = -1001205498567;
+$authorizedChats = array( $group_PogoTube42, $group_NordEstLegit, $bot_Exeggutor, $group_admin );
 $authorizedUsers = array( 'Trial4life', 'DadyGC', 'medix93');
 
 
@@ -95,7 +96,7 @@ elseif(strpos($text, "/quests") === 0 ) {
 	$response = 'Elenco delle quests di oggi:';
 	for ($i = 0; $i <= sizeof($quest)-1; $i++){
 		$link = 'https://maps.google.com/?q='.$lat[$i].','.$lng[$i];
-		$response = $response . "\n*" . $quest[$i] . "* − [" . $pokestop[$i] . "](" . $link . ")";
+		$response = $response . "\n*" . ucfirst($quest[$i]) . "* − [" . $pokestop[$i] . "](" . $link . ")";
 	}
 
 	$parameters = array('chat_id' => $chatId, "text" => $response, "parse_mode" => "markdown", "disable_web_page_preview" => TRUE);
