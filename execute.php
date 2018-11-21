f9e0d9b5f4a881316facccd42cb232fef6001983
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-11-21 20:03:39 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -201,7 +201,7 @@ elseif($status == 0)
 						$link = 'https://maps.google.com/?q='.$lat.','.$lng;
 						$data = [
 		   		 		'chat_id' => $channel,
-		   		 		'text' => 'Quest *'. $quest . '* \n Pokéstop: [' . $pkst . '](' . $link . ')',
+		   		 		'text' => 'Quest *'. $quest . "*\nPokéstop: [" . $pkst . '](' . $link . ')',
 		   	 			'parse_mode' => 'markdown',
 		   	 			'disable_web_page_preview' => TRUE,
 		   			];
