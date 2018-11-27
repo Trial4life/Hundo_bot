7a7dfd70b4829feeef5bd4a75eeb53c011fd3279
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-11-27 19:53:52 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -128,7 +128,7 @@ elseif(strpos($text, "/quests") === 0 ) {
 	// ELENCO QUESTS
 	mysqli_query($conn,"DELETE FROM `quests` WHERE giorno < '$today'");  // RIMUOVE LE QUEST DEL GIORNO PRECEDENTE
 	// $query = "SELECT * FROM `quests` ORDER BY quests ASC";
-	$query = "SELECT * FROM `quests` ORDER BY quests ASC";
+	$query = "SELECT * FROM `quests`";
 	$result_quest = mysqli_query($conn,$query);
 	$quest = $pokestop = $lat = $lng = array();
 	while ($row = mysqli_fetch_assoc($result_quest)) {
