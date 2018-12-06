b0d5b3f1636137401cb4902582b902751f9bb781
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-12-06 10:14:43 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -286,7 +286,7 @@ elseif($status == 0)
 	if(strpos($text, "/quest ") === 0 )	{
 		if (in_array($chatId, $authorizedChats)) {
 			$data = [
-	   	 	'chat_id' => $userId,
+	   	 	'chat_id' => $chatId,
 	   	 	'text' => $EMO_EXE.' Per segnalare una quest, utilizza il comando /quests in chat privata con @Exeggutor_bot.',
 	   	 	'parse_mode' => 'markdown',
 			];
