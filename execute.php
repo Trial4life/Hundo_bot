b8f285a9f505ed36361b7bc875d3873d1f0baf19
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-12-09 21:50:50 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -493,7 +493,7 @@ elseif($status == 2) {
 				$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
 			}
 
-			$response = $EMO_v.' La quest è stata registrata.'.$userAlertsIDs[0];
+			$response = $EMO_v.' La quest è stata registrata.';
 			$parameters = array('chat_id' => $userId, "text" => $response, "parse_mode" => "markdown");
 			$parameters["method"] = "sendMessage";
 			echo json_encode($parameters);
