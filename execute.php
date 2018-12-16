19ecdc4dbd975a55f78e6c667b57ac8fff784abc
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-12-16 01:50:21 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -552,7 +552,7 @@ elseif($status == 0) {
 		$result = mysqli_query($conn,$query);
 		$row = mysqli_fetch_assoc($result);
 		if (!$row) {
-			$response = $EMO_v.' La cella *'.$cellId.'* è stata registrata.';
+			$response = $EMO_v." La cella *'.$cellId.'* è stata registrata come *\"".$name."\"*.";
 			mysqli_query($conn,"INSERT INTO `zones` (cellId, cellId64, name, groups) VALUES ('$cellId', '$cellId64', '$name', '')");
 			$response = mysqli_error($conn);
 		}
