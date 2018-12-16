7ee859463e07d41d96a8496d069fc9bd75265778
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-12-16 16:56:08 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -719,7 +719,7 @@ elseif($status == 2) {
 			}
 //////
 
-			$response = $EMO_v.' La quest è stata registrata.'.json_encode($groupsIDs).$groupsIDs[0];
+			$response = $EMO_v.' La quest è stata registrata.';
 			$parameters = array('chat_id' => $userId, "text" => $response, "parse_mode" => "markdown");
 			$parameters["method"] = "sendMessage";
 			echo json_encode($parameters);
