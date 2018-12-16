cb6acf228da36e9472f3ab7745ffb9f8d1899980
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-12-16 19:49:57 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -371,7 +371,7 @@ elseif($status == 0) {
 				$quest = ucfirst(str_replace('/quest ', '', $text));
 				$data = [
 		   	 	'chat_id' => $chatId,
-		   	 	'text' => $EMO_PIN.' @'.$username.', mandami la posizione della quest *'.$quest.'* tramite @ingressportalbot.',
+		   	 	'text' => $EMO_PIN.' '.$firstname.', mandami la posizione della quest *'.$quest.'* tramite @ingressportalbot.',
 		   	 	'parse_mode' => 'markdown',
 				];
 				$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
