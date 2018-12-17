92483b880737bcc0b79e098258ac1752d7f78a81
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-12-17 21:41:30 +0100

    Update authorizations.php

--- authorizations.php
+++ authorizations.php
@@ -1,11 +1,5 @@
 <?php
 $conn2 = new mysqli("db4free.net", "trial4life", "16021993", "tradepkmn");
-	$bot_Exeggutor = 158754689;
-	$group_PogoTube42 = -1001204753064;
-	$group_NordEstLegit = -1001119443518;
-	$group_admin = -1001205498567;
-	$group_SalarioParioli = -1001369640732;
-
 	$query = "SELECT * FROM `auth_groups`";
 	$result = mysqli_query($conn2,$query);
 	$row = mysqli_fetch_assoc($result);
@@ -26,6 +20,12 @@ $conn2 = new mysqli("db4free.net", "trial4life", "16021993", "tradepkmn");
 	}
 
 /*
+	$bot_Exeggutor = 158754689;
+	$group_PogoTube42 = -1001204753064;
+	$group_NordEstLegit = -1001119443518;
+	$group_admin = -1001205498567;
+	$group_SalarioParioli = -1001369640732;
+
 	$authorizedChats = array(
 		$group_PogoTube42,
 		$group_NordEstLegit,
