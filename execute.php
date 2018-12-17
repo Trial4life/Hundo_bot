b8d6779220eeed46377451f556f309c3f3c7dff6
Trial4life <33653846+Trial4life@users.noreply.github.com>
2018-12-17 20:56:07 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -767,10 +767,10 @@ elseif($status == 0) {
 	//////////////////
 	elseif(strpos($text, "/addadmin") === 0 ) {
 		if (in_array($username, $admins)) {
-			$admin = explode(', ', str_replace('/addadmin ', '', $text));
-			mysqli_query($conn,"INSERT INTO `admins` VALUES ('$admin[1]')");
+			$admin = str_replace('/addadmin ', '', $text);
+			mysqli_query($conn,"INSERT INTO `admins` VALUES ('$admin')");
 
-			$response = $EMO_v.' '.$admin[1].' aggiunto come admin.';
+			$response = $EMO_v.' '.$admin.' aggiunto come admin.';
 			$parameters = array('chat_id' => $chatId, "text" => $response, "parse_mode" => "markdown", "disable_web_page_preview" => TRUE);
 			$parameters["method"] = "sendMessage";
 			echo json_encode($parameters);
