f58206e2c0c7c00226b95e4de7f002e4b2568d1e
Trial4life <33653846+Trial4life@users.noreply.github.com>
2019-01-14 21:31:40 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1057,8 +1057,8 @@ elseif($status == 2) {
 				if ($userAlertsID == $userId) {
 					$data = [
 			  			'chat_id' => $userAlertsID,
-			  			// 'text' => "`Quest:   ` *". $quest . "*\n`Pokéstop:` [" . str_replace("\'","'",$pkst) . "](" . $link . ")\n`Giorno:  ` ".$today2."\n`Task:    ` ". $task,
-			  			'text' => '<code>prova</code>',
+			  			 //'text' => "`Quest:   ` *". $quest . "*\n`Pokéstop:` [" . str_replace("\'","'",$pkst) . "](" . $link . ")\n`Giorno:  ` ".$today2."\n`Task:    ` ". $task,
+			  			'text' => "<code>Quest:</code>". $quest ."<br><code>Pokéstop:</code>".str_replace("\'","'",$pkst)."<br><code>Giorno:</code>".$today2."<br><code>Task:</code>".$task,
 			  			//'text' => "`Quest:   ` *". $quest . "*\n`Pokéstop:` [" . $pkst . "](" . $link . ")\n`Giorno:  ` ".$today2,
 			  			'parse_mode' => 'HTML',
 			  			// 'parse_mode' => 'markdown',
