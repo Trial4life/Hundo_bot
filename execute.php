30f159567474baca6f42364dac538304f58ec1f5
Trial4life <33653846+Trial4life@users.noreply.github.com>
2019-01-15 00:41:04 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1029,7 +1029,7 @@ elseif($status == 2) {
 		$om_pkst = $row['pokestop'];
 		$om_lat = $row['lat'];
 		$om_lng = $row['lng'];
-		$link = 'https://maps.google.com/?q='.$lat.','.$lng.'('.str_replace("\'","'",$pkst).')';		// BETA
+		$link = "https://maps.google.com/?q=".$lat.",".$lng."(".str_replace("\'","'",$pkst).")";		// BETA
 		// $link = 'https://maps.google.com/?q='.$lat.','.$lng;
 		if ($om_pkst == str_replace("\'","'",$pkst) and $om_lat == $lat and $om_lng == $lng) {				// IN REALTÀ BISOGNEREBBE FAR EIL CONFRONTO CON TUTTI GLI OMONINI! CI VUOLE while
 			// AVVISO DI QUEST GIÀ SEGNALATA
@@ -1059,7 +1059,7 @@ elseif($status == 2) {
 			  			'chat_id' => $userAlertsID,
 			  			 //'text' => "`Quest:   ` *". $quest . "*\n`Pokéstop:` [" . str_replace("\'","'",$pkst) . "](" . $link . ")\n`Giorno:  ` ".$today2."\n`Task:    ` ". $task,
 			  			//'text' => "<code>Quest:</code>". $quest ."<br><code>Pokéstop:</code>".str_replace("\'","'",$pkst)."<br><code>Giorno:</code>".$today2."<br><code>Task:</code>".$task,
-			  			'text' => "<code>Quest:    </code><b>". $quest ."</b>\n<code>Pokéstop: </code><a href=".$link.">".str_replace("\'","'",$pkst)."</a>\n<code>Giorno:   </code>".$today2."\n<code>Task:     </code>".$task,
+			  			'text' => "<code>Quest:    </code><b>". $quest ."</b>\n<code>Pokéstop: </code><a href='".$link."'>".str_replace("\'","'",$pkst)."</a>\n<code>Giorno:   </code>".$today2."\n<code>Task:     </code>".$task,
 
 			  			//'text' => "`Quest:   ` *". $quest . "*\n`Pokéstop:` [" . $pkst . "](" . $link . ")\n`Giorno:  ` ".$today2,
 			  			'parse_mode' => 'HTML',
