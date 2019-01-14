af4f5eae4da011892a8af9fe489fc713b3cb0153
Trial4life <33653846+Trial4life@users.noreply.github.com>
2019-01-15 00:36:33 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1059,7 +1059,7 @@ elseif($status == 2) {
 			  			'chat_id' => $userAlertsID,
 			  			 //'text' => "`Quest:   ` *". $quest . "*\n`Pokéstop:` [" . str_replace("\'","'",$pkst) . "](" . $link . ")\n`Giorno:  ` ".$today2."\n`Task:    ` ". $task,
 			  			//'text' => "<code>Quest:</code>". $quest ."<br><code>Pokéstop:</code>".str_replace("\'","'",$pkst)."<br><code>Giorno:</code>".$today2."<br><code>Task:</code>".$task,
-			  			'text' => "<code>Quest:    </code>". $quest ."\n<code>Pokéstop: </code><a href = 'prova'>".str_replace("\'","'",$pkst)."</a>\n<code>Giorno:   </code>".$today2."\n<code>Task:     </code>".$task,
+			  			'text' => "<code>Quest:    </code>". $quest ."\n<code>Pokéstop: </code><a href='https://maps.google.com/?q=2,2(prova)'>".str_replace("\'","'",$pkst)."</a>\n<code>Giorno:   </code>".$today2."\n<code>Task:     </code>".$task,
 
 			  			//'text' => "`Quest:   ` *". $quest . "*\n`Pokéstop:` [" . $pkst . "](" . $link . ")\n`Giorno:  ` ".$today2,
 			  			'parse_mode' => 'HTML',
