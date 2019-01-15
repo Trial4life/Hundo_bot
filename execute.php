d7978e2a3c8c7a9bbfa1267c930a50c79fb0a713
Trial4life <33653846+Trial4life@users.noreply.github.com>
2019-01-15 12:28:29 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -642,7 +642,7 @@ elseif($status == 0) {
 		$currNest = $row['nido'];
 
 		// setlocale(LC_ALL, "ita");
-		$endDate = date("j/m", strtotime(str_replace('-','/', $endDate)));
+		$endDate = str_replace(" ","",date("j/m", strtotime(str_replace('-','/', $endDate))));
 
 		if ($currNest == $nest) {
 			$response = 'Il nido a *'.$nest.'* è stato già segnalato fino al *'.$endDate.'*.';
@@ -702,7 +702,7 @@ elseif($status == 0) {
 		}
 
 		// setlocale(LC_ALL, "ita");
-		$endDate = date("j/m", strtotime(str_replace('-','/', $endDate)));
+		$endDate = str_replace(" ","",date("j/m", strtotime(str_replace('-','/', $endDate))));
 
 		if (!$nest) {
 			$response = 'Nessun nido segnalato fino al *'.$endDate.'*.';
