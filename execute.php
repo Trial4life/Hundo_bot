b838d2b2f5217baa57bb365a90657a69c33639f1
Trial4life <33653846+Trial4life@users.noreply.github.com>
2019-01-17 20:23:03 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -666,7 +666,16 @@ elseif($status == 0) {
 	/// METEO ///
 	/////////////
 	elseif(strpos($text, "/meteo") === 0 ){
-
+		$inline_keyboard = [
+		    new InlineKeyboardButton(['text' => 'inline', 'switch_inline_query' => 'true']),
+		    new InlineKeyboardButton(['text' => 'callback', 'callback_data' => 'identifier']),
+		    new InlineKeyboardButton(['text' => 'open url', 'url' => 'https://github.com/akalongman/php-telegram-bot']),
+		];
+		$data = [
+		    'chat_id' => $chat_id,
+		    'text'    => 'inline keyboard',
+		    'reply_markup' => new InlineKeyboardMarkup(['inline_keyboard' => [$inline_keyboard]]),
+		];
 	}
 
 ###################
