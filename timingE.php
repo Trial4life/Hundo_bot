16e489c8fbc2987e8dd4c38821e479f9404c9c12
Trial4life <33653846+Trial4life@users.noreply.github.com>
2019-01-19 17:48:58 +0100

    4

--- timingE.php
+++ timingE.php
@@ -1,7 +1,7 @@
 				<?php
 					$data = [
 					  	'chat_id' => $userId,
-					  	'text' => date("H:m:s"),
+					  	'text' => 'Start: '.date("H:i:s"),
 					];
 					$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
 				?>
---
+++ timingS.php
@@ -0,0 +1,7 @@
+				<?php
+					$data = [
+					  	'chat_id' => $userId,
+					  	'text' => 'End: '.date("H:i:s"),
+					];
+					$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
+				?>
