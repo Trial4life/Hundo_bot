d2059555bdbc19af88a28f80327091a167efd5d6
Trial4life <33653846+Trial4life@users.noreply.github.com>
2019-01-24 10:28:47 +0100

    Create unregister.php

---
+++ unregister.php
@@ -0,0 +1,18 @@
+<?php
+
+// PARAMETRI DA MODIFICARE
+// $WEBHOOK_URL = 'https://hundo.herokuapp.com/execute.php';
+$WEBHOOK_URL = 'https://2.227.251.71:443/_DEV_/[HEROKU]/Exeggutor_bot/hundo_bot/execute.php';
+$BOT_TOKEN = '689487990:AAGhqhcsalt0mXYRnUqFro9ECNxPuOOVPZc';
+
+// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+$method = 'deleteWebhook';
+$parameters = array('url' => $WEBHOOK_URL);
+$url = $API_URL . $method. '?' . http_build_query($parameters);
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 0);
+curl_setopt($handle, CURLOPT_TIMEOUT, 120);
+$result = curl_exec($handle);
+print_r($result);
