bff79d2414e83b583f37983f9cdb68c35c4c53ca
Trial4life <33653846+Trial4life@users.noreply.github.com>
2019-01-25 10:20:19 +0100

    Update weather.php

--- weather.php
+++ weather.php
@@ -1,7 +1,10 @@
 <?php
-   date_default_timezone_set('Europe/Rome');		// Nel main è dichiarato "London", qui serve "Rome" per gli orari di alba e tramonto
-	$sunriseHour = intval(substr(date_sunrise(time(), SUNFUNCS_RET_STRING, 41.893056, 12.482778, 90, 2),0,-2)) -1;		// -1 è per un bug di date_sunrise?
-	$sunsetHour = intval(substr(date_sunset(time(), SUNFUNCS_RET_STRING, 41.893056, 12.482778, 90, 2),0,-2)) -1;		// -1 è per un bug di date_sunset?
+   //date_default_timezone_set('Europe/Rome');		// Nel main è dichiarato "London", qui serve "Rome" per gli orari di alba e tramonto
+   $dtz = new DateTimeZone('Europe/Rome');
+	$time_Rome = new DateTime('now', $dtz);
+	$offset = $dtz->getOffset( $time_Rome ) / 3600;
+	$sunriseHour = intval(substr(date_sunrise(time(), SUNFUNCS_RET_STRING, 41.893056, 12.482778, 90, $offset),0,-2));
+	$sunsetHour  = intval(substr( date_sunset(time(), SUNFUNCS_RET_STRING, 41.893056, 12.482778, 90, $offset),0,-2));
 	$now = date('G');
 
 	// Cells //
