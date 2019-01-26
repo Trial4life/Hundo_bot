c3b4900d2004410e82edf29de46b13841f60a3b7
Trial4life <33653846+Trial4life@users.noreply.github.com>
2019-01-27 00:58:30 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1195,7 +1195,7 @@ elseif($status == 0) {
 		for ($i = 0; $i <= sizeof($trainer)-1; $i++){
 			// $tgLink = "[".$telegram[$i]."](tg://user?id=".$telegram[$i].")";
 			// $tgLink = "[".$telegram[$i]."]"."(https://web.telegram.org/#/im?p=@".$telegram[$i].")";
-			$tgLink = "[".$telegram[$i]."](https://t.me/".$telegramId[$i].")";
+			$tgLink = "[".$telegram[$i]."](tg://user?id=".$telegramId[$i].")";
 			$response = $response."\n*".$trainer[$i]."* − ".$tgLink."\n`".$code[$i].'`';
 		}
 		$parameters = array('chat_id' => $chatId, "text" => $response, "parse_mode" => "markdown", "disable_web_page_preview" => TRUE);
